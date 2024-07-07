ff43df42bae22a1faa4d13550f9f93c07cf99ce9
Essien Emmanuel <essienemma300@gmail.com>
2024-07-07 21:42:22 +0100

    worked on adding image file and styled the product create page

--- src/routes/admin/product.js
+++ src/routes/admin/product.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+const multer = require("multer");
 
 const { validationResult } = require("express-validator");
 const ProductRepo = require("../../repositories/product");
 const productsNewTemplate = require("../../views/admin/product/new");
 const { requireTitle, requirePrice } = require("./validators");
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -11,9 +14,24 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.send(errors);
-  res.send("submitted");
-});
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.send(productsNewTemplate({ errors }));
+
+    const { title, price } = req.body;
+    const image = req.file.buffer.toString("base64");
+
+    const newProduct = await ProductRepo.create({
+      title,
+      price,
+      image,
+    });
+
+    res.send("submitted");
+  }
+);
 module.exports = router;
